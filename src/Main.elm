e4988640e33be9e976d9049e43cb8524d5fbed23
batfink <vnystad@gmail.com>
2018-11-22 20:40:03 +0000

    chore: add stuff

---
+++ src/Main.elm
@@ -0,0 +1,14 @@
+module Main exposing (..)
+
+import Html exposing (text)
+
+ask : String -> String -> String
+ask thing place =
+    "is there a "
+        ++ thing
+        ++ " in the "
+        ++ place
+        ++ "?"
+
+main =
+    text <| ask "fish" "sock"
